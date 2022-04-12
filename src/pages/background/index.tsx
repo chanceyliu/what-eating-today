57853c2a5296db0542b0d582fabd9f2a54193c8e
chanceyliu <1445885764@qq.com>
2022-04-12 11:09:25 +0800

    style: update wep icon

--- src/pages/background/index.tsx
+++ src/pages/background/index.tsx
@@ -1,5 +1,5 @@
 import { FC, useMemo } from 'react'
-import styles from './index.module.less'
+// import styles from './index.module.less'
 import cherry from '@/assets/icons/cherry.png'
 import corn from '@/assets/icons/corn.png'
 import kiwi from '@/assets/icons/kiwi.png'
@@ -35,15 +35,25 @@ const Index: FC = () => {
             width: `${100 / BG_COLUMN}%`,
             height: '100vh',
             backgroundColor: parentIndex % 2 === 0 ? 'rgb(198, 124, 136)' : 'pink',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            position: 'relative',
+            overflow: 'hidden',
+            textAlign: 'center',
           }}
         >
           {/* 因为我们只需要内部元素滚动，而不是带背景颜色的div滚动，所以外层加一个div */}
           {/* 参考：https://juejin.cn/post/6941242335443288077 */}
-          <div className={styles.columns}>
+          <div>
+            {new Array(iconNumber + 1).fill('').map((_, childIndex) => (
+              <img
+                key={childIndex}
+                width={iconSize}
+                src={
+                  iconList[
+                    parentIndex >= iconList.length ? parentIndex % iconList.length : parentIndex
+                  ]
+                }
+                style={{ padding: '5px 0 5px 0' }}
+              />
+            ))}
             {new Array(iconNumber + 1).fill('').map((_, childIndex) => (
               <img
                 key={childIndex}
